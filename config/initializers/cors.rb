2c7845920187d9f39d0070a8214894ce8a921e69
travisim <64481469+travisim@users.noreply.github.com>
2024-07-01 17:33:37 +0800

    added seed and fixed cors for render deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001','http://frontend.localhost'
+      origins 'http://localhost:3001','http://frontend.localhost','https://curvify-frontend.onrender.com'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
